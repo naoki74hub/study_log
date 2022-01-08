ca144c5a097568d5f258833e235dc838860e6a4a
naoki74hub <nao.ryotaro.1112@icloud.com>
2022-01-08 13:29:38 +0900

    TaskControllderのindex修正

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,16 +10,14 @@ use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    public function index(int $id)
+    public function index(Folder $folder)
     {
-        
         $folders = Folder::all();
-        $current_folder = Folder::find($id);
-        $tasks = $current_folder->tasks()->get();
+        $tasks = $folder->tasks()->get();
         
         return view('tasks/index', [
              'folders' => $folders,
-             'current_folder_id' => $current_folder->id,
+             'current_folder_id' => $folder->id,
              'tasks' => $tasks,
         ]);
     }
